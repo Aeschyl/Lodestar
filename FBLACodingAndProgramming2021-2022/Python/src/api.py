95d535994e1a400dff93b3e3859a00e2021fe4d6
Aryamann Sheoran <aryamannsheoran@gmail.com>
2021-12-17 13:37:27 -0700

    adding basic python folder

---
+++ FBLACodingAndProgramming2021-2022/Python/src/api.py
@@ -0,0 +1,12 @@
+import requests
+from os import getenv
+from requests.structures import CaseInsensitiveDict
+
+# The variables below will be passed to python through the options.json file
+url = 'https://api.geoapify.com/v2/places?categories=' + category +  '&filter=' + location  + '&limit=' + limit + '&apiKey=' + getenv('PLACES_TOKEN')
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+
+# The data received from the API is passed to C# through the response.json file
+resp = requests.get(url=url, headers=headers)
