7743342f7f4c58bd844974058332914ac743f962
Abhiram Kasu <abhiram.kasu@gmail.com>
2021-12-18 17:48:10 -0700

    finished first Model View and First Page for Category

--- FBLACodingAndProgramming2021-2022/Python/src/api.py
+++ FBLACodingAndProgramming2021-2022/Python/src/api.py
@@ -1,12 +1,25 @@
 import requests
 from os import getenv
 from requests.structures import CaseInsensitiveDict
+import json
 
-# The variables below will be passed to python through the Input.json file
-url = 'https://api.geoapify.com/v2/places?categories=' + category +  '&filter=' + location  + '&limit=' + limit + '&apiKey=' + getenv('PLACES_TOKEN')
+#defining dummy parameters, will get actual data from Input.json later
+category = 'accommodation.hotel'
+location = 'rect:11.549881365729718,48.15114774722076,11.58831616443861,48.12837326392079'
+limit = '20'
+
+# Defining the url to request the data from
+url = 'https://api.geoapify.com/v2/places?categories=' + category +  '&filter=' + location  + '&limit=' + limit + '&apiKey=' + '233315ef9be94184b7addc54c006bbde' # <-- Places API key
 
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 
-# The data received from the API is passed to C# through the Output.json file
-resp = requests.get(url=url, headers=headers)
+# The data received from the API
+response = requests.get(url=url, headers=headers).json()
+
+# Serializing json 
+json_object = json.dumps(response, indent = 4)
+  
+# The data received from the API is passed to C# through the output.json file
+with open("./Output/Output.json", "w") as outfile:
+    outfile.write(json_object)
